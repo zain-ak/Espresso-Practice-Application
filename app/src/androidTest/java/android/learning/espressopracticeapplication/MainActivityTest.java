24f02f4894447bdfb921f5b2a25a00a84696bc07
Zain Khalid <zak565@uowmail.edu.au>
2019-03-04 20:48:18 -0500

    - Completed @Test greeting() to check that:     - TextView is empty at start of app;     - after Button performs click, TextView is updated w/text 'Well well      well...'.
    - Created Button greetingButton in MainActivity and corresponding
      resource greeting_button
    - Added onClick attribute in resource file to map to
      setGreetingView(View) method in Main Activity

--- app/src/androidTest/java/android/learning/espressopracticeapplication/MainActivityTest.java
+++ app/src/androidTest/java/android/learning/espressopracticeapplication/MainActivityTest.java
@@ -7,6 +7,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
@@ -14,7 +19,17 @@ public class MainActivityTest {
     @Rule   //JUnit Test Rule, Run for every test method
     public ActivityTestRule<MainActivity> activityRule =  new ActivityTestRule(MainActivity.class);
 
-    @Test
-    public void setGreetingView() {
+
+    @Test //Check that initial greeting is empty.
+    public void greeting() {
+        onView(withId(R.id.greeting_view))
+                .check(matches(withText("")));
+
+        onView(withId(R.id.greeting_btn))
+                .perform(click());
+
+        onView(withId(R.id.greeting_view))
+                .check(matches(withText("Well well well...")));
+
     }
 }
--- app/src/main/java/android/learning/espressopracticeapplication/MainActivity.java
+++ app/src/main/java/android/learning/espressopracticeapplication/MainActivity.java
@@ -2,22 +2,26 @@ package android.learning.espressopracticeapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView greetingView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         greetingView = findViewById(R.id.greeting_view);
+        Button greetingBtn = findViewById(R.id.greeting_btn);
 
     }
 
-    protected void setGreetingView() {
+    //Keep public so that MainActivity Test can access this method
+    public void setGreetingView(View view) {
         greetingView.setText("Well well well...");
     }
-
 }
