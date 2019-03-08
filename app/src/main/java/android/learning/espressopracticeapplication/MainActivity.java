7a7402d624f3a7cad8deece1ab6285b40573c0c8
Zain Khalid <zak565@uowmail.edu.au>
2019-03-08 05:51:12 -0500

    activity_main: - added resources for a ListView and a header. These were added to   continue the video tutorial being followed. - header used instead of footer as I couldn't get the latter to show on   the screen due to layout issues
    MainActivity:
    - added code for the resources added in activity_main. Instead of creat-
      ing a new project as the videos series does, I integrated the new code
      with the existing project code
    - On clicking the button, the button and textview are hidden from view
      and the listview and header are made visible (invisible upon app
      start)
    
    * All code has been run manually and is running correctly. any quirks
    have been dealt with. Next commit will look at implementing the
    espresso testing for the code added in this session.
    
    Again, this will be looking to integrate the new test with an existing
    test or something like that.

--- app/src/main/java/android/learning/espressopracticeapplication/MainActivity.java
+++ app/src/main/java/android/learning/espressopracticeapplication/MainActivity.java
@@ -1,15 +1,24 @@
 package android.learning.espressopracticeapplication;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView greetingView;
+    private TextView greetingView, header;
     private Button greetingBtn;
+    private ListView list;
+
+    private final int COUNT = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +28,54 @@ public class MainActivity extends AppCompatActivity {
         greetingView = findViewById(R.id.greeting_view);
         greetingBtn = findViewById(R.id.greeting_btn);
 
+        header = findViewById(R.id.header);
+        header.setBackgroundColor(Color.YELLOW);
+        header.setVisibility(View.GONE);
+
+        list = findViewById(R.id.list);
+        list.setVisibility(View.GONE);
+        final ArrayList<Items> arrayList = new ArrayList<>(COUNT);
+        for (int i = 0; i <= COUNT; i++) {
+            arrayList.add(i, new Items(i));
+        }
+        ArrayAdapter<Items> listAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
+        list.setAdapter(listAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i == COUNT) {
+                    greetingView.setVisibility(View.VISIBLE);
+                    greetingBtn.setVisibility(View.VISIBLE);
+                    greetingBtn.setEnabled(true);
+                    list.setVisibility(View.GONE);
+                    header.setText(arrayList.get(i).toString());
+                }
+                else {
+                    header.setText(arrayList.get(i).toString());
+                    header.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     //Keep public so that MainActivity Test can access this method
     public void setGreetingView(View view) {
         greetingBtn.setEnabled(false);
         greetingView.setText(R.string.textview_text);
+        greetingView.setVisibility(View.GONE);
+        greetingBtn.setVisibility(View.GONE);
+        list.setVisibility(View.VISIBLE);
+    }
+
+    public class Items {
+        private int value;
+
+        Items(int x) {value = x;}
+
+        @Override
+        public java.lang.String toString() {
+            return Integer.toString(value);
+        }
     }
 }
